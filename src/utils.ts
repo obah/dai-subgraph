144ceb5157aa41991d002a3195737c0a522351e1
obah <olusojiobah@gmail.com>
2024-10-05 09:51:13 +0100

    feat: implemented dai subgraph

---
+++ src/utils.ts
@@ -0,0 +1,13 @@
+import { BigInt } from "@graphprotocol/graph-ts";
+import { User } from "../generated/schema";
+
+export function getUser(id: string): User {
+  let user = User.load(id);
+  if (user === null) {
+    user = new User(id);
+    user.balance = BigInt.fromI32(0);
+    user.save();
+  }
+
+  return user;
+}
